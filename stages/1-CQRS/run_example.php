5bcb8096ddda339f6fcf21acf504e78ae6c9b0fc
Dariusz Gafka <dariuszgafka@gmail.com>
2023-03-25 15:29:46 +0100

    run example

--- stages/1-CQRS/run_example.php
+++ stages/1-CQRS/run_example.php
@@ -1,11 +1,31 @@
 <?php
 
+use App\Application\Command\ChangeUserName;
+use App\Application\Command\RegisterUser;
+use App\Domain\User\UserName;
+use App\Domain\User\UserRepository;
+use App\Infrastructure\InMemoryUserRepository;
 use Ecotone\Lite\EcotoneLiteApplication;
+use PHPUnit\Framework\Assert;
+use Ramsey\Uuid\Uuid;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$ecotoneLite = EcotoneLiteApplication::bootstrap();
+$ecotoneLite = EcotoneLiteApplication::bootstrap(pathToRootCatalog: __DIR__, classesToRegister: [UserRepository::class => new InMemoryUserRepository()]);
 
+/** In case of using Symfony or Laravel, Buses will be automatically available in your dependency container */
 $commandBus = $ecotoneLite->getCommandBus();
 $queryBus = $ecotoneLite->getQueryBus();
 
+$userId = Uuid::uuid4();
+$expectedName = UserName::create('John Doe');
+
+echo "Starting example. \n";
+$commandBus->send(new RegisterUser($userId, UserName::create('John Snow')));
+$commandBus->send(new ChangeUserName($userId, $expectedName));
+
+Assert::assertEquals(
+    $expectedName,
+    $queryBus->sendWithRouting('user.getName', $userId)
+);
+echo "Example finished with success.\n";
